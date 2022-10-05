05930eefe110988809f31e9a12f7d69bb210b47e
ThanhOwO <91793770+ThanhOwO@users.noreply.github.com>
2022-10-05 12:07:04 +0700

    make it better

--- Nodejs/index.js
+++ Nodejs/index.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 5000
 const mongoose = require('mongoose')
 const authRouter = require('./models/routes/auth')
 const postRouter = require('./models/routes/post')
@@ -36,8 +35,8 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
 })
  
 connectDB()
